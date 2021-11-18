fbe60ab79ecfb6e2990393a32953bbb493be885d
Kapil <kapil.nagar@zeapl.com>
2021-11-18 13:01:40 +0530

    fixes

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -52,7 +52,7 @@ class Report extends CI_Controller {
   $sql="SELECT vch.party_code,im.user_name,vch.point,vch.id_rate,ex.Exchange_Code,sum(vch.gross_amount) as  gross_amount,sum(vch.pati) as pati,sum(vch.commision) as commision,sum(final_amount) as final_amount from voucher_details vch join Entry  ent on vch.vouchar_id=ent.id join Idmaster im on im.id= vch.user_id join Exchange ex on ex.id = im.exch_code and vch.client_id=$client_id group by vch.user_id ";    
      $query = $this->db->query($sql);
     $data['result']= $query->result_array();
-         $this->load->view('admin/Report/Spartywise',$data);
+         $this->load->view('admin/Report/spartywise',$data);
           $this->load->view('admin/assets/footer');
     }else{
           redirect('Client');
